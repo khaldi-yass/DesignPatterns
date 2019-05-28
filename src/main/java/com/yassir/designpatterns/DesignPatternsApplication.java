63109037f70e91384ca57fef6a8a5b99c8f44f3c
Yassir Kh <khaldi.yass@gmail.com>
2019-05-28 12:34:36 +0000

    Initial Commit

---
+++ src/main/java/com/yassir/designpatterns/DesignPatternsApplication.java
@@ -0,0 +1,17 @@
+package com.yassir.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsApplication.class, args);
+        /**
+         * This project explains and implement the most common design patterns in the industry
+         * Open to changes and updates
+         */
+    }
+
+}
